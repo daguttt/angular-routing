3e1f20dee4eb35e6cb8e87a2195bb72990521668
Daniel Gutierrez Muñoz <dagutmu667@gmail.com>
2022-10-18 18:53:37 -0500

    task 3 solved

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,17 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
   title = 'routing-angular';
+
+  constructor(private router: Router) {}
+
+  queryParams() {
+    this.router.navigate(['countries'], { queryParams: { name: 'colombia' } });
+  }
 }
